b422409f680f717d900c5691f02976ff910fc1ce
Nuzkito <nuzkito+github@gmail.com>
2025-11-24 22:21:38 +0100

    Add compatibility with game build 700386

--- Patches.cs
+++ Patches.cs
@@ -5,33 +5,47 @@ namespace Unlock_Blueprints
 {
     public class Patches
     {
-        [HarmonyPatch(typeof(Blueprints_U51AndBefore))]
-        [HarmonyPatch("SetupBlueprints")]
-        public class Blueprints_U51AndBefore_SetupBlueprints_Patch
+        [HarmonyPatch(typeof(BuildingFacadeInfo), "get_rarity")]
+        public static class BuildingFacadeInfo_Rarity_Patch
         {
-            public static void Postfix(ref Blueprints_U51AndBefore __instance)
+            public static bool Prefix(ref PermitRarity __result)
             {
-                foreach (var buildingFacade in __instance.blueprintCollection.buildingFacades)
-                {
-                    buildingFacade.rarity = PermitRarity.Universal;
-                }
+                __result = PermitRarity.Universal;
 
-                foreach (var artable in __instance.blueprintCollection.artables)
-                {
-                    artable.rarity = PermitRarity.Universal;
-                }
+                return false;
+            }
+        }
 
+        [HarmonyPatch(typeof(ArtableInfo), "get_rarity")]
+        public static class ArtableInfo_Rarity_Patch
+        {
+            public static bool Prefix(ref PermitRarity __result)
+            {
+                __result = PermitRarity.Universal;
 
-                foreach (var clothingItem in __instance.blueprintCollection.clothingItems)
-                {
-                    clothingItem.rarity = PermitRarity.Universal;
-                }
+                return false;
+            }
+        }
 
+        [HarmonyPatch(typeof(ClothingItemInfo), "get_rarity")]
+        public static class ClothingItemInfo_Rarity_Patch
+        {
+            public static bool Prefix(ref PermitRarity __result)
+            {
+                __result = PermitRarity.Universal;
 
-                foreach (var balloonArtistFacade in __instance.blueprintCollection.balloonArtistFacades)
-                {
-                    balloonArtistFacade.rarity = PermitRarity.Universal;
-                }
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(BalloonArtistFacadeInfo), "get_rarity")]
+        public static class BalloonArtistFacadeInfo_Rarity_Patch
+        {
+            public static bool Prefix(ref PermitRarity __result)
+            {
+                __result = PermitRarity.Universal;
+
+                return false;
             }
         }
     }
